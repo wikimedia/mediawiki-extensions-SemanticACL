71ef53859cfc0f27027235968bc64573963ed92d
Sébastien Beyou <sebastien.beyou@wiki-valley.com>
2022-02-21 18:56:08 +0100

    Replace deprecated Content::getNativeData
    Bug:  T283691
    Change-Id: I45b7cfd9c67886ce8e27380aa0462811ba9e046b

--- SemanticACL.class.php
+++ SemanticACL.class.php
@@ -17,6 +17,7 @@
 namespace MediaWiki\Extension\SemanticACL;
 
 use Article;
+use LogicException;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Revision\SlotRecord;
 use RequestContext;
@@ -24,6 +25,7 @@ use SMW;
 use SMWDIProperty;
 use SMWDIWikiPage;
 use SMWQueryResult;
+use TextContent;
 use Title;
 
 /**
@@ -409,8 +411,18 @@ class SemanticACL {
 					// Use a new parser to avoid interfering with the current parser.
 					$parser = \MediaWiki\MediaWikiServices::getInstance()->getParserFactory()->create();
 					$parser->startExternalParse( $title, \ParserOptions::newFromContext( RequestContext::getMain() ), \Parser::OT_PREPROCESS );
+					$revision = Article::newFromTitle( $title, RequestContext::getMain() )->getPage()->getRevisionRecord();
+					if ( !$revision ) {
+						// Should not happen
+						throw new LogicException( 'SemanticACL: unknown revision with the \'key\' mechanism' );
+					}
+					$content = $revision->getContent( SlotRecord::MAIN );
+					if ( !( $content instanceof TextContent ) ) {
+						// Not implemented on non-text contents
+						break;
+					}
 					$text = $parser->recursivePreprocess(
-						Article::newFromTitle( $title, RequestContext::getMain() )->getPage()->getRevisionRecord()->getContent( SlotRecord::MAIN )->getNativeData(),
+						$content->getText(),
 						$title,
 						$parser->mOptions
 					);
